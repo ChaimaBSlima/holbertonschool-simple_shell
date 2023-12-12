4adccf429c3c4941c941bca575565813cbfacceb
ChaimaBSlima <chaimabslima@outlook.fr>
2023-12-12 11:54:43 +0100

    handeling betty

--- exec.c
+++ exec.c
@@ -1,18 +1,19 @@
 #include "simple_shell.h"
 /**
- * 
- * main : Entry point
- * 
- * 
- * 
-*/
-int main (void){
+ * main - Entry point
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
 	char commands[100];
-	while(1){
+
+	while (1)
+	{
 		write(STDOUT_FILENO, "$", strlen("$"));
 		read_command(commands, sizeof(commands));
-        execute_command(commands);
-
+		execute_command(commands);
 	}
 	return (0);
 }
--- execute_commands.c
+++ execute_commands.c
@@ -1,46 +1,72 @@
 #include "simple_shell.h"
-
-void read_command(char *command, size_t size) {
-    if (fgets(command, size, stdin) == NULL) {
-        if (feof(stdin)) {
-            leo_print("\n");
-            exit(EXIT_SUCCESS);
-        } else {
-            leo_print("Error while reading input.\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-    command[strcspn(command, "\n")] = '\0'; // Remove newline
+/**
+ * read_command - Read commands from user
+ *
+ * @command: command from the user
+ * @size: size of the command
+ * Return: Void
+ *
+ */
+void read_command(char *command, size_t size)
+{
+	if (fgets(command, size, stdin) == NULL)
+	{
+		if (feof(stdin))
+		{
+			write(STDOUT_FILENO, "\n", strlen("\n"));
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			write(STDOUT_FILENO, "Error while reading input.\n",
+			 strlen("Error while reading input.\n"));
+			exit(EXIT_FAILURE);
+		}
+	}
+	command[strcspn(command, "\n")] = '\0';
 }
-void execute_command(const char *command) {
+/**
+ * execute_command - excute the commands typed
+ *  by the user
+ *
+ * @command: command type by the user
+ * Return: Void
+ *
+ */
+void execute_command(const char *command)
+{
 
-    __pid_t child_pid = fork();
+	__pid_t child_pid = fork();
 
-    if (child_pid == -1) {
-        leo_print("Error forking process.\n");
-        exit(EXIT_FAILURE);
-    } else if (child_pid == 0) {
-        // Child process
+	if (child_pid == -1)
+	{
+		write(STDOUT_FILENO, "Error forking process.\n",
+		 strlen("Error forking process.\n"));
+		exit(EXIT_FAILURE);
+	}
+	else if (child_pid == 0)
+	{
 
-        // Parse the command and its arguments
-        char *args[128]; // Maximum 128 arguments (adjust as needed)
-        int arg_count = 0;
+		char *args[128];
+		int arg_count = 0;
+		char *token = strtok((char *)command, " ");
 
-        char *token = strtok((char *)command, " ");
-        while (token != NULL) {
-            args[arg_count++] = token;
-            token = strtok(NULL, " ");
-        }
-        args[arg_count] = NULL; // Null-terminate the arguments array
+		while (token != NULL)
+		{
+			args[arg_count++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[arg_count] = NULL;
 
-        // Execute the command
-        execvp(args[0], args);
 
-        // If execvp fails, print an error message
-        leo_print("Error executing command.\n");
-        exit(EXIT_FAILURE);
-    } else {
-        // Parent process
-        wait(NULL);
-    }
+		execvp(args[0], args);
+
+		write(STDOUT_FILENO, "Error executing command.\n",
+		strlen("Error executing command.\n"));
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(NULL);
+	}
 }
